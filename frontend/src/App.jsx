cec26363cb7e055e7e0defb622a66ffba12d789d
stalerico <sarina.talerico06@gmail.com>
2026-01-17 17:32:23 -0500

    frontend stuff

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,3 @@
-import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import { Routes, Route, useNavigate } from "react-router-dom";
@@ -6,17 +5,12 @@ import './App.css'
 import Test from "./pages/Test.jsx";
 import Home from "./pages/Home.jsx";
 
-
-
 function App() {
-  const navigate = useNavigate();
   return (
-    <>
     <Routes>
       <Route path="/" element={<Home />} />"
       <Route path="/test" element={<Test />} />
     </Routes>
-    </>
   );
 }
 
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,11 +1,18 @@
-import TypingAnimatedText from "./components/TypingAnimatedText.tsx";
+import TypingAnimatedText from "../components/TypingAnimatedText";
+import { useNavigate } from "react-router-dom";
+import { useState } from 'react'
+import CursorEffect from "../components/CursorEffect.tsx";
+
 export default function Home() {
+  const navigate = useNavigate();
   return (
     <div>
+      <CursorEffect />
       <h1>Welcome to the Home Page</h1>
       <p>This is the main landing page of the application.</p>
      <button onClick={() => navigate("/test")}>Click me woof woof</button>
-    <TypingAnimatedText />
+     
+      <TypingAnimatedText />
     </div>
   );
 }
