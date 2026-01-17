6725a9f99e8cc8846e6fe58ee80b38d6368da75a
cinnacodes <cxinaxmon@gmail.com>
2026-01-17 17:11:50 -0500

    merge

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,7 +4,7 @@ import viteLogo from '/vite.svg'
 import { Routes, Route, useNavigate } from "react-router-dom";
 import './App.css'
 import Test from "./pages/Test.jsx";
-import TypingAnimatedText from "./components/TypingAnimatedText.tsx";
+
 
 
 function App() {
@@ -12,9 +12,9 @@ function App() {
   return (
     <>
     <h1>Hello Vite + React!</h1>
-    <button onClick={() => navigate("/test")}>Click me woof woof</button>
-    <TypingAnimatedText />
+
     <Routes>
+      <Route path"/" element={<Home"
       <Route path="/test" element={<Test />} />
     </Routes>
     </>
